012b2b9fa0faa8efdda39052a2dd19f95a853a08
Meganathan S V <s.sivachidambaram+meganathan+github@agriya.in>
2016-02-08 19:39:54 +0530

    Fixes #191: Zapier app implementaion

--- server/php/oauth_callback.php
+++ server/php/oauth_callback.php
@@ -22,7 +22,17 @@ if (!empty($_GET['plugin'])) {
         'client_secret' => $data['settings'][$_GET['plugin'] . '_client_secret']['value'],
         'code' => $_GET['code']
     );
-    $response = doPost($data['settings'][$_GET['plugin'] . '_oauth_token_url']['value'], $post_data, 'token');
+    $_server_domain_url = '';
+    if ($_GET['plugin'] == 'r_zapier') {
+        $_server_protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http';
+        $_server_domain_url = $_server_protocol . '://' . $_SERVER['HTTP_HOST'];
+        $post_data['redirect_uri'] = $_server_domain_url . '/client/apps/r_zapier/login.html';
+    }
+    $format = ($_GET['plugin'] == 'r_zapier') ? 'json' : 'token';
+    $response = doPost($_server_domain_url . $data['settings'][$_GET['plugin'] . '_oauth_token_url']['value'], $post_data, $format);
+    if (is_array($response)) {
+        $response = json_encode($response);
+    }
     $response_array = json_decode($response, true);
     if (json_last_error() == JSON_ERROR_NONE) {
         $access_token = $response_array['access_token'];
