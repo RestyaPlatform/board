62a7ee4c4182c5301daacdf924f3f3134007d45b
Meganathan S V <s.sivachidambaram+meganathan+github@agriya.in>
2016-02-08 12:57:21 +0530

    - Card due date notification mail implemented

---
+++ server/php/shell/card_due_notification.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Cron to card due date
+ *
+ * PHP version 5
+ *
+ * @category   PHP
+ * @package    Restyaboard
+ * @subpackage Core
+ * @author     Restya <info@restya.com>
+ * @copyright  2014-2016 Restya
+ * @license    http://restya.com/ Restya Licence
+ * @link       http://restya.com/
+ */
+$app_path = dirname(dirname(__FILE__));
+require_once $app_path . '/config.inc.php';
+global $_server_domain_url;
+if (file_exists(APP_PATH . '/tmp/cache/site_url_for_shell.php')) {
+    include_once APP_PATH . '/tmp/cache/site_url_for_shell.php';
+}
+if ($db_lnk) {
+    $qry_val_arr = array();
+    $result = pg_query_params($db_lnk, "SELECT * FROM cards_listing WHERE due_date::DATE = (select DATE 'tomorrow')", $qry_val_arr);
+    while ($card = pg_fetch_assoc($result)) {
+        $cards_users = json_decode($card['cards_users']);
+        foreach ($cards_users as $cards_user) {
+            $mail_content = '';
+            if (!empty($cards_user->profile_picture_path)) {
+                $hash = md5(SECURITYSALT . 'User' . $cards_user->user_id . 'png' . 'small_thumb' . SITE_NAME);
+                $profile_picture_path = $_server_domain_url . '/img/small_thumb/User/' . $cards_user->user_id . '.' . $hash . '.png';
+                $user_avatar = '<img style="margin-right: 10px;vertical-align: middle;" src="' . $profile_picture_path . '" alt="[Image: ' . $cards_user->full_name . ']" class="img-rounded img-responsive">' . "\n";
+            } else if (!empty($cards_user->initials)) {
+                $user_avatar = '<i style="border-radius:4px;text-shadow:#6f6f6f 0.02em 0.02em 0.02em;width:32px;height:32px;line-height:32px;font-size:16px;display:inline-block;font-style:normal;text-align:center;text-transform:uppercase;color:#f47564 !important;background-color:#ffffff !important;border:1px solid #d7d9db;margin-right: 10px;">' . $cards_user->initials . '</i>' . "\n";
+            }
+            $message = 'Gentle reminder the card due date is tomorrow (<a href="' . $_server_domain_url . '/#/board/' . $card['board_id'] . '/card/' . $card['id'] . '">' . $card['name'] . '</a> on <a href="' . $_server_domain_url . '/#/board/' . $card['board_id'] . '">' . $card['board_name'] . '</a>)';
+            $br = '<div style="line-height:40px;">&nbsp;</div>';
+            $mail_content.= '<div style="display:table">' . "\n";
+            $mail_content.= '<div style="float:left">' . $user_avatar . '</div>' . "\n";
+            $mail_content.= '<div style="vertical-align: middle;display:table-cell;">' . $message . '</div>' . "\n";
+            $mail_content.= '</div>' . "\n";
+            $mail_content.= $br . "\n";
+            $users[$cards_user->email][] = $mail_content;
+        }
+    }
+    if (!empty($users)) {
+        foreach ($users as $key => $val) {
+            $emailFindReplace['##CONTENT##'] = $val;
+            sendMail('due_date_notification', $emailFindReplace, $key);
+        }
+    }
+}
