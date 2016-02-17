9e1cc56631404b154dd2debd5f3fedcc9317c9ce
Baskar P <s.sivachidambaram+baskar+github@agriya.in>
2016-02-17 16:40:43 +0530

    Fixes #218: Need notification highlight for username mentions

--- client/js/views/footer_view.js
+++ client/js/views/footer_view.js
@@ -825,6 +825,7 @@ App.FooterView = Backbone.View.extend({
                             model: activity,
                             type: 'all'
                         });
+						$('.js-unread-activity').parent().addClass('bg-danger navbar-btn'); 
                         if (mode == 1) {
                             view_activity.prepend(view.render().el).find('.timeago').timeago();
                         } else {
@@ -1340,6 +1341,7 @@ App.FooterView = Backbone.View.extend({
                             model: activity,
                             board: self.board
                         });
+						$('.js-unread-activity').parent().addClass('bg-danger navbar-btn'); 
                         if ($('.js-list-activity-' + activity.id, view_activity).length === 0) {
                             view_activity.append(view.render().el).find('.timeago').timeago();
                         }
@@ -1614,7 +1616,8 @@ App.FooterView = Backbone.View.extend({
                             model: activity,
                             board: self.board,
                             type: modeType
-                        });
+                        });   
+						$('.js-unread-activity').parent().addClass('bg-danger navbar-btn');
                         view_activity.append(view.render().el).find('.timeago').timeago();
                     });
                 } else {
