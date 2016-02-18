4a8320cd44a3ccd158e17e3a3b2a73eec3b902a1
Sridhar V <s.sivachidambaram+sridhar+github@agriya.in>
2016-02-18 19:09:25 +0530

    Changes #222: Need a button to delete all archived items => Delete all cards & lists from object and also need to add alert

---
+++ client/js/views/archive_card_delete_confirm_view.js
@@ -0,0 +1,74 @@
+/**
+ * @fileOverview This file has functions related to list delete confirm view. This view calling from list view.
+ * Available Object:
+ *	App.boards						: this object contain all boards(Based on logged in user)
+ *	this.model						: list model. It contain all list based object @see Available Object in App.ListView
+ */
+if (typeof App == 'undefined') {
+    App = {};
+}
+/**
+ * ListDeleteConfirm View
+ * @class ArchiveDeleteConfirmView
+ * @constructor
+ * @extends Backbone.View
+ */
+App.ArchiveCardDeleteConfirmView = Backbone.View.extend({
+    /**
+     * Constructor
+     * initialize default values and actions
+     */
+    initialize: function() {
+        if (!_.isUndefined(this.model) && this.model !== null) {
+            this.model.showImage = this.showImage;
+        }
+        this.render();
+    },
+    template: JST['templates/archive_card_delete_confirm'],
+    tagName: 'div',
+
+    events: {
+        'click .js-delete-all-archived-cards-delete': 'deleteAllArchivedCards',
+        'click .js-close-popup': 'closePopup',
+    },
+    deleteAllArchivedCards: function(e) {
+        var self = this;
+        self.model.url = api_url + 'boards/' + self.model.id + '/cards.json';
+        self.model.destroy({
+            success: function(model, response) {
+                self.model.attributes.lists.forEach(function(list, index) {
+                    _.each(list.cards, function(card, key) {
+                        if (!_.isEmpty(card)) {
+                            if (!_.isUndefined(card.is_archived) && parseInt(card.is_archived) === 1) {
+                                self.model.attributes.lists[index].cards.splice(key, 1);
+                            }
+                        }
+                    });
+                });
+                self.flash('success', i18next.t('Cards deleted successfully.'));
+                $('.js-archived-items').trigger('click');
+            }
+        });
+        console.log(self.model);
+        return false;
+    },
+    closePopup: function(e) {
+        var target = $(e.target);
+        target.parents('li.dropdown:first, div.dropdown:first').removeClass('open');
+        return false;
+    },
+    /**
+     * render()
+     * populate the html to the dom
+     * @param NULL
+     * @return object
+     *
+     */
+    render: function() {
+        this.$el.html(this.template({
+            list: this.model
+        }));
+        this.showTooltip();
+        return this;
+    }
+});
