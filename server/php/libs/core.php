f427712284e5ed864d56efe78025b4f1f789d84f
Meganathan S V <s.sivachidambaram+meganathan+github@agriya.in>
2016-02-18 15:58:47 +0530

    Changes #129: In organization, Remove functionality is not working - fixed

--- server/php/libs/core.php
+++ server/php/libs/core.php
@@ -515,6 +515,7 @@ function checkAclLinks($r_request_method = 'GET', $r_resource_cmd = '/users', $r
 	$tem_arr = array('/boards_users/?');
 	$board_exception_arr = array('/boards/?');
 	$organization_exception_arr = array('/organizations/?');
+	$organization_exception_method_arr = array('DELETE', 'PUT');
 	//temp fix
 	if((!empty($r_resource_vars['boards']) && !in_array($r_resource_cmd, $board_exception_arr))  || in_array($r_resource_cmd, $tem_arr)) {
 		if ($r_request_method == 'PUT' && in_array($r_resource_cmd, $tem_arr)) {
@@ -535,7 +536,7 @@ function checkAclLinks($r_request_method = 'GET', $r_resource_cmd = '/users', $r
 		if (empty($board_allowed_link)) {
 			 return false;
 		}
-	} else if (!empty($r_resource_vars['organizations']) && !in_array($r_resource_cmd, $organization_exception_arr)) {
+	} else if (!empty($r_resource_vars['organizations']) && (!in_array($r_resource_cmd, $organization_exception_arr) || (in_array($r_resource_cmd, $organization_exception_arr) && in_array($r_request_method, $organization_exception_method_arr)))) {
 		$qry_val_arr = array(
 			$r_resource_vars['organizations'],
 			$authUser['id']
