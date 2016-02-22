e3badb33d77f3411851525ae6b5626979c4047c5
Meganathan S V <s.sivachidambaram+meganathan+github@agriya.in>
2016-02-22 16:24:50 +0530

    Fixes #290: After dropped the file change background not working

--- client/js/views/board_header_view.js
+++ client/js/views/board_header_view.js
@@ -55,6 +55,14 @@ App.BoardHeaderView = Backbone.View.extend({
         this.model.labels.bind('change', this.showFilters, this);
         this.authuser = authuser.user;
         this.renderAdminBoardUsers();
+        if (!_.isUndefined(authuser.user)) {
+            var board_user_role_id = this.model.board_users.findWhere({
+                user_id: parseInt(authuser.user.id)
+            });
+            if (!_.isEmpty(board_user_role_id)) {
+                this.model.board_user_role_id = board_user_role_id.attributes.board_user_role_id;
+            }
+        }
     },
     /**
      * Events
