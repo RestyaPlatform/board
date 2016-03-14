6781b7dfcb38a57c1ce260514e47e33972b0e954
Meganathan S V <s.sivachidambaram+meganathan+github@agriya.in>
2016-03-14 15:29:38 +0530

    Fixes #436: LDAP setting upgrade is not smooth.

--- sql/upgrade-0.2-0.2.1.sql
+++ sql/upgrade-0.2-0.2.1.sql
@@ -1,2 +1,3 @@
 UPDATE "settings" SET "name" = 'elasticsearch.last_processed_activity_id' WHERE "name" = 'elasticsearch.last_processed_activtiy_id';
 UPDATE "settings" SET "name" = 'webhooks.last_processed_activity_id' WHERE "name" = 'webhooks.last_processed_activtiy_id';
+UPDATE settings SET value = replace(value, 'ldap://', '') WHERE name = 'LDAP_SERVER';
