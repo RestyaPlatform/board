29b883100ecc1c65bb469ccc2c03c19cf276133d
Meganathan S V <s.sivachidambaram+meganathan+github@agriya.in>
2016-03-18 19:56:43 +0530

    Fixes #495: Admin sort boards by created and last activity

--- client/js/collections/board_collection.js
+++ client/js/collections/board_collection.js
@@ -23,6 +23,41 @@ App.BoardCollection = Backbone.Collection.extend({
         this.sortField = field;
         this.sortDirection = direction;
     },
+    comparator: function(item) {
+        if (!_.isUndefined(this.sortDirection)) {
+            var self = this;
+            var str = '' + item.get(this.sortField);
+            if (this.sortField !== 'id') {
+                str = str.toLowerCase();
+                str = str.split('');
+                str = _.map(str, function(letter) {
+                    if (self.sortDirection.toLowerCase() === 'desc') {
+                        return String.fromCharCode(-(letter.charCodeAt(0)));
+                    } else {
+                        return String.fromCharCode((letter.charCodeAt(0)));
+                    }
+                });
+                return str;
+            } else if (this.sortField === 'created' || this.sortField === 'modified') {
+                if (item.get(this.sortField) !== null) {
+                    var date = item.get(this.sortField).split(' ');
+                    if (!_.isUndefined(date[1])) {
+                        _date = date[0] + 'T' + date[1];
+                    } else {
+                        _date = date[0];
+                    }
+                    sort_date = new Date(_date);
+                    return self.sortDirection.toLowerCase() === 'desc' ? -sort_date.getTime() : sort_date.getTime();
+                }
+            } else {
+                if (self.sortDirection.toLowerCase() === 'desc') {
+                    return -item.get(this.sortField);
+                } else {
+                    return item.get(this.sortField);
+                }
+            }
+        }
+    },
     parse: function(response) {
         if (!_.isUndefined(response._metadata)) {
             return response.data;
