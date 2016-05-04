9d286ed2f82c2918a8e29bb7d9846263335e464d
Jawahar G <s.sivachidambaram+jawahar+github@agriya.in>
2016-05-04 11:29:56 +0530

    Changes #587: Altered card elastic search listing view

--- sql/upgrade-0.2.1-0.3.sql
+++ sql/upgrade-0.2.1-0.3.sql
@@ -1207,3 +1207,61 @@ CREATE VIEW "cards_elasticsearch_listing" AS
            FROM ((cards cards
       LEFT JOIN boards boards ON ((boards.id = cards.board_id)))
    LEFT JOIN lists lists ON ((lists.id = cards.list_id)))) card;
+   
+DROP VIEW "cards_elasticsearch_listing";
+
+CREATE VIEW "cards_elasticsearch_listing" AS
+    SELECT card.id, 
+    row_to_json(card.*) AS json
+   FROM ( SELECT cards.id, 
+            cards.board_id, 
+            boards.name AS board, 
+            cards.list_id, 
+            lists.name AS list, 
+            cards.name, 
+            cards.description, 
+            to_char(cards.due_date, 'YYYY-MM-DD"T"HH24:MI:SS'::text) AS due_date, 
+            to_char(cards.created, 'YYYY-MM-DD"T"HH24:MI:SS'::text) AS created, 
+            to_char(cards.modified, 'YYYY-MM-DD"T"HH24:MI:SS'::text) AS modified, 
+            (cards.is_archived)::integer AS is_archived, 
+            cards.attachment_count, 
+            cards.checklist_item_count, 
+            cards.checklist_item_completed_count, 
+            cards.card_voter_count, 
+			cards.cards_user_count, 
+            ( SELECT array_to_json(array_agg(row_to_json(cc.*))) AS array_to_json
+                   FROM ( SELECT boards_users.user_id
+                           FROM boards_users boards_users
+                          WHERE (boards_users.board_id = cards.board_id)
+                          ORDER BY boards_users.id) cc) AS board_users, 
+            ( SELECT array_to_json(array_agg(row_to_json(cc.*))) AS array_to_json
+                   FROM ( SELECT board_stars.user_id
+                           FROM board_stars board_stars
+                          WHERE (board_stars.board_id = cards.board_id)
+                          ORDER BY board_stars.id) cc) AS board_stars, 
+            ( SELECT array_to_json(array_agg(row_to_json(cc.*))) AS array_to_json
+                   FROM ( SELECT checklists.name, 
+                            checklist_items.name AS checklist_item_name
+                           FROM (checklists checklists
+                      LEFT JOIN checklist_items checklist_items ON ((checklist_items.checklist_id = checklists.id)))
+                     WHERE (checklists.card_id = cards.id)
+                     ORDER BY checklists.id) cc) AS cards_checklists, 
+            ( SELECT array_to_json(array_agg(row_to_json(cc.*))) AS array_to_json
+                   FROM ( SELECT cards_users_listing.username, 
+                            cards_users_listing.user_id
+                           FROM cards_users_listing cards_users_listing
+                          WHERE (cards_users_listing.card_id = cards.id)
+                          ORDER BY cards_users_listing.id) cc) AS cards_users, 
+            ( SELECT array_to_json(array_agg(row_to_json(cl.*))) AS array_to_json
+                   FROM ( SELECT cards_labels.name
+                           FROM cards_labels_listing cards_labels
+                          WHERE (cards_labels.card_id = cards.id)
+                          ORDER BY cards_labels.id) cl) AS cards_labels, 
+            ( SELECT array_to_json(array_agg(row_to_json(cl.*))) AS array_to_json
+                   FROM ( SELECT activities.comment
+                           FROM activities activities
+                          WHERE (((activities.type)::text = 'add_comment'::text) AND (activities.card_id = cards.id))
+                          ORDER BY activities.id) cl) AS activities
+           FROM ((cards cards
+      LEFT JOIN boards boards ON ((boards.id = cards.board_id)))
+   LEFT JOIN lists lists ON ((lists.id = cards.list_id))) WHERE boards.name IS NOT NULL) card;
