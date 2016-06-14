7021d87e5e2c6ca8e430cb33eabc2aaaa0737138
marikannan l <marikannanl308@gmail.com>
2016-06-14 16:59:56 +0530

    Changes #684: ejabberd user, board, and its members add php script integrated

--- server/php/shell/upgrade_v0.2.1_v0.3.php
+++ server/php/shell/upgrade_v0.2.1_v0.3.php
@@ -14,6 +14,14 @@
  */
 $app_path = dirname(dirname(__FILE__));
 require_once $app_path . '/config.inc.php';
+require_once $app_path . '/libs/vendors/finediff.php';
+require_once $app_path . '/libs/core.php';
+require_once $app_path . '/libs/vendors/OAuth2/Autoloader.php';
+require_once $app_path . '/libs/vendors/xmpp/vendor/autoload.php';
+use Xmpp\Xep\Xep0045 as xmpp;
+use Psr\Log\LoggerInterface;
+require $app_path . '/libs/vendors/jaxl3/jaxl.php';
+
 // Delete ElasticSearch index
 $ch = curl_init(ELASTICSEARCH_URL . ELASTICSEARCH_INDEX);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
@@ -24,3 +32,73 @@ $qry_val_arr = array(
     'elasticsearch.last_processed_activity_id'
 );
 pg_query_params($db_lnk, 'UPDATE settings SET value = $1 WHERE name = $2', $qry_val_arr);
+
+$j_username = $j_password = '';
+$users = pg_query_params($db_lnk, 'SELECT * FROM users', array());
+while ($user = pg_fetch_assoc($users)) {
+	if (JABBER_HOST) {
+		$conditions = array(
+			$user['username'],
+			$user['password']
+		);
+		$chat_db_lnk = getEjabberdConnection();
+		$check_user = pg_query_params($chat_db_lnk, 'SELECT password FROM users WHERE username = $1 and password = $2', $conditions);
+		if (!($check_user && pg_num_rows($check_user))) {
+			global $j_username, $j_password;
+			$jaxl_initialize = array(
+				'jid' => JABBER_HOST,
+				'strict' => false,
+				'log_level' => JAXL_DEBUG,
+				'port' => 5222,
+				'log_path' => 'jaxl.log'
+			);
+			$GLOBALS['client'] = new JAXL($jaxl_initialize);
+			$j_username = $user['username'];
+			$j_password = $user['password'];
+			$xeps = array(
+				'0077'
+			);
+			$GLOBALS['client']->require_xep($xeps);
+			$GLOBALS['client']->add_cb('on_stream_features', function ($stanza)
+			{
+				global $argv;
+				$GLOBALS['client']->xeps['0077']->get_form(JABBER_HOST);
+				return "wait_for_register_form";
+			});
+			$GLOBALS['client']->add_cb('on_disconnect', function ()
+			{
+				global $form;
+				_info("registration " . ($form['type'] == 'result' ? 'succeeded' : 'failed'));
+			});
+			$GLOBALS['client']->start();
+			$xmpp_user = array(
+				'username' => $user['username'] . '@' . JABBER_HOST,
+				'password' => $user['password'],
+				'host' => JABBER_HOST,
+				'ssl' => false,
+				'port' => 5222,
+				'resource' => uniqid('', true)
+			);
+			$xmpp = new xmpp($xmpp_user);
+			$board_conditions = array(
+				$user['id'],
+				'f'
+			);
+			$boards = pg_query_params($db_lnk, 'SELECT id,name FROM boards WHERE user_id = $1 and is_closed = $2', $board_conditions);
+			if ($boards && pg_num_rows($boards)) {
+                while ($board = pg_fetch_object($boards)) {
+					$xmpp->createRoom('board-' . $board['id'], $board['name']);
+					$board_members_conditions = array(
+						$board['id']
+					);
+					$board_members = pg_query_params($db_lnk, 'SELECT u.username FROM boards_users bu left join users u on u.id = bu.user_id WHERE board_id = $1', $board_members_conditions);
+					if ($board_members && pg_num_rows($board_members)) {
+						while ($board_member = pg_fetch_object($board_members)) {
+							$xmpp->grantMember('board-' . $board['id'], $board_member['username'], 'member');
+						}
+					}
+				}
+			}
+		}
+	}
+}
