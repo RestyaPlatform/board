58471a91dd828a691414f351b8ef414a6b714f67
Marikannan.L <l.marikannan@agriya.in>
2016-08-27 17:52:22 +0530

    Changes #672: Multi-User - Boards not Updating Live - issue fixed

--- sql/upgrade-0.3-0.3.1.sql
+++ sql/upgrade-0.3-0.3.1.sql
@@ -269,3 +269,52 @@ CREATE OR REPLACE VIEW "cards_listing" AS
      LEFT JOIN users u ON ((u.id = cards.user_id)))
      LEFT JOIN boards b ON ((b.id = cards.board_id)))
      LEFT JOIN lists l ON ((l.id = cards.list_id)));
+
+CREATE OR REPLACE VIEW "activities_listing" AS
+ SELECT activity.id,
+    to_char(activity.created, 'YYYY-MM-DD"T"HH24:MI:SS'::text) AS created,
+    to_char(activity.modified, 'YYYY-MM-DD"T"HH24:MI:SS'::text) AS modified,
+    activity.board_id,
+    activity.list_id,
+    activity.card_id,
+    activity.user_id,
+    activity.foreign_id,
+    activity.type,
+    activity.comment,
+    activity.revisions,
+    activity.root,
+    activity.freshness_ts,
+    activity.depth,
+    activity.path,
+    activity.materialized_path,
+    board.name AS board_name,
+    list.name AS list_name,
+    card.name AS card_name,
+    users.username,
+    users.full_name,
+    users.profile_picture_path,
+    users.initials,
+    la.name AS label_name,
+    card.description AS card_description,
+    users.role_id AS user_role_id,
+    checklist_item.name AS checklist_item_name,
+    checklist.name AS checklist_item_parent_name,
+    checklist1.name AS checklist_name,
+    organizations.id AS organization_id,
+    organizations.name AS organization_name,
+    organizations.logo_url AS organization_logo_url,
+    list1.name AS moved_list_name,
+    to_char(activity.created, 'HH24:MI'::text) AS created_time,
+    card.position AS card_position,
+    card.comment_count AS comment_count
+   FROM ((((((((((activities activity
+     LEFT JOIN boards board ON ((board.id = activity.board_id)))
+     LEFT JOIN lists list ON ((list.id = activity.list_id)))
+     LEFT JOIN lists list1 ON ((list1.id = activity.foreign_id)))
+     LEFT JOIN cards card ON ((card.id = activity.card_id)))
+     LEFT JOIN labels la ON (((la.id = activity.foreign_id) AND ((activity.type)::text = 'add_card_label'::text))))
+     LEFT JOIN checklist_items checklist_item ON ((checklist_item.id = activity.foreign_id)))
+     LEFT JOIN checklists checklist ON ((checklist.id = checklist_item.checklist_id)))
+     LEFT JOIN checklists checklist1 ON ((checklist1.id = activity.foreign_id)))
+     LEFT JOIN users users ON ((users.id = activity.user_id)))
+     LEFT JOIN organizations organizations ON ((organizations.id = activity.organization_id)));
