038cc6671955b534adefc9805a6b932270423d47
Marikannan L <marikannanl308@gmail.com>
2016-10-17 20:52:29 +0530

    Fixes #846: Upgrade from version 0.2.1 to 0.3 fails

--- sql/upgrade-0.3-0.3.1.sql
+++ sql/upgrade-0.3-0.3.1.sql
@@ -22,6 +22,7 @@ Restyaboard<br>
 </footer>
 </body>
 </html>' WHERE "name" = 'welcome';
+
 UPDATE "email_templates" SET "email_text_content" = '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 <body style="margin:0">
@@ -46,6 +47,7 @@ Restyaboard<br>
 </footer>
 </body>
 </html>' WHERE "name" = 'changepassword';
+
 UPDATE "email_templates" SET "email_text_content" = '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 <body style="margin:0">
@@ -71,6 +73,7 @@ Restyaboard<br>
 </footer>
 </body>
 </html>' WHERE "name" = 'newprojectuser';
+
 UPDATE "email_templates" SET "email_text_content" = '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 <body style="margin:0">
@@ -98,6 +101,7 @@ UPDATE "email_templates" SET "email_text_content" = '<html>
 </footer>
 </body>
 </html>' WHERE "name" = 'email_notification';
+
 UPDATE "email_templates" SET "email_text_content" = '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 <body style="margin:0">
@@ -123,6 +127,7 @@ Restyaboard<br>
 </footer>
 </body>
 </html>' WHERE "name" = 'activation';
+
 UPDATE "email_templates" SET "email_text_content" = '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 <body style="margin:0">
@@ -147,6 +152,7 @@ Restyaboard<br>
 </footer>
 </body>
 </html>' WHERE "name" = 'forgetpassword';
+
 UPDATE "email_templates" SET "email_text_content" = '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 <body style="margin:0">
@@ -270,4 +276,7 @@ CREATE OR REPLACE VIEW "cards_listing" AS
      LEFT JOIN boards b ON ((b.id = cards.board_id)))
      LEFT JOIN lists l ON ((l.id = cards.list_id)));
 
-update users set timezone = '+0530';
+UPDATE users SET timezone = '+0530';
+
+DELETE FROM setting_categories WHERE id = 13;
+DELETE FROM settings WHERE id >= 52;
