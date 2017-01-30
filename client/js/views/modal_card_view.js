04a1a490bfdd74b267d06cba2e4c984e88fadcbf
mohan_361at15 <n.mohanamurali@agriya.in>
2017-01-30 16:56:53 +0530

    fixes #982 : URL Routing Bug code mofied added

--- client/js/views/modal_card_view.js
+++ client/js/views/modal_card_view.js
@@ -918,9 +918,7 @@ App.ModalCardView = Backbone.View.extend({
                         } else if (current_param.indexOf('/card/' + self.model.id) != -1) {
                             current_param = current_param.replace('/card/' + self.model.id, '');
                         } else {
-                            var currenturl = window.location;
-                            var currentss = currenturl.hash;
-                            var board_id = currentss.split("/");
+                            var board_id = window.location.hash.split("/");
                             current_param = 'board/' + board_id['2'];
                         }
                         app.navigate('#/' + current_param, {
