9338840f15eb0befff252e30f933f43dac30020e
mohan_361at15 <n.mohanamurali@agriya.in>
2017-02-02 16:04:48 +0530

    changes #1001: email placeholder issuse fixed

--- server/php/authorize.php
+++ server/php/authorize.php
@@ -95,7 +95,12 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 	 </script>
 <?php
 // display an authorization form
-if (!empty($error_msg) && (empty($_POST['authorized']) || (!empty($_POST['authorized']) && $_POST['authorized'] === 'Deny'))) { ?>
+if (!empty($error_msg) && (empty($_POST['authorized']) || (!empty($_POST['authorized']) && $_POST['authorized'] === 'Deny'))) { 
+	if (is_plugin_enabled('r_ldap_login')) {
+        $loginPlaceholder = 'LDAP Login';
+    } else {
+        $loginPlaceholder = 'Email or Username';
+    }?>
 	<section class="clearfix">
 	  <div class="col-md-5 col-md-offset-4">
 		<div class="text-center navbar-btn"><a title="Restya" href="#/"><img title="<?php
@@ -109,7 +114,7 @@ if (!empty($error_msg) && (empty($_POST['authorized']) || (!empty($_POST['author
 						<form class="form-horizontal clearfix col-xs-12" method="post" role="form" name="UserLoginForm" id="UserLoginForm">
 							<div class="form-group">
 							  <label for="inputEmail" class="sr-only control-label">Email or Username</label>
-							  <input type="text" placeholder="Email or Username" class="form-control authorize_ldap change-ldap" id="inputEmail" name="email"  value="<?php
+							  <input type="text" placeholder="<?php echo $loginPlaceholder; ?>" class="form-control authorize_ldap change-ldap" id="inputEmail" name="email"  value="<?php
     echo !empty($_POST['email']) ? $_POST['email'] : ''; ?>" title="" required/>
 							</div>
 							<div class="form-group">
