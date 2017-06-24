2df36dffe0b31ca3f49870d22ac99bce86f8bf88
Ahsan <l.marikannan@agriya.in>
2017-06-24 17:27:48 +0530

    unnecessary file deleted

--- sql/upgrade-0.4.1-0.5.sql
+++
@@ -1,5 +0,0 @@
-UPDATE "acl_board_links" SET "url" = '/boards/?/lists/?/cards/?/cards_users/?' WHERE "slug" = 'delete_card_user';
-
-SELECT pg_catalog.setval('acl_board_links_boards_user_roles_seq', (SELECT MAX(id) FROM acl_board_links_boards_user_roles), true);
-
-delete from settings where name='STANDARD_LOGIN_ENABLED';
