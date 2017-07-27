3dffbdf812a1c929bbbe7034d93829cb9787ce74
Ahsan <l.marikannan@agriya.in>
2017-07-27 17:54:15 +0530

    issue #1256: Enhancement: better decoding on imap.php

--- server/php/shell/imap.php
+++ server/php/shell/imap.php
@@ -32,7 +32,6 @@ if (!$connection) {
 }
 $message_count = imap_num_msg($connection);
 for ($counter = 1; $counter <= $message_count; $counter++) {
-    // Fetch email header
     $header = imap_header($connection, $counter);
     foreach($header->to as $to) {
         $mail = explode('+', $to->mailbox);
@@ -41,8 +40,21 @@ for ($counter = 1; $counter <= $message_count; $counter++) {
         // Check to email address contains atleast one "+" symbol
         if (count($mail) > 1) {
             // Fetch email body
-            $body = imap_fetchbody($connection, $counter, 1);
-
+            $s = imap_fetchstructure($connection,$counter);
+            if (!$s->parts)  // simple
+                $body_data = imapBodyDecode($connection, $counter, $s, 0);  // pass 0 as part-number
+            else {  // multipart: cycle through each part
+                foreach ($s->parts as $partno0=>$p) {
+                    $body_data[] = imapBodyDecode($connection, $counter, $p,$partno0+1);
+                }
+            }
+            if($body_data) {
+                if($body_data[0] && is_array($body_data[0])) {
+                    $body = $body_data[0][0];
+                } else {
+                    $body = $body_data[0];
+                }
+            }
             $board_id = $mail[1];
             $card_id = '';
             $hash = $mail[2];
@@ -74,12 +86,13 @@ for ($counter = 1; $counter <= $message_count; $counter++) {
                         $card_query = pg_query_params('SELECT ' . $str . ' as position FROM cards WHERE board_id = $1 AND list_id = $2', $val_arr);
                         $card = pg_fetch_assoc($card_query);
                         $position = empty($card['position']) ? 1 : (!empty($board['is_default_email_position_as_bottom'])) ? $card['position'] + 1 : ($card['position'] / 2);
-                        $title = decode_qprint($header->subject);
+                        // Fetch email header
+                        $title = imap_utf8($header->subject);
                         $val_arr = array(
                             $board_id,
                             $list_id,
                             $title,
-                            decode_qprint($body) ,
+                            $body ,
                             $position,
                             $board['user_id']
                         );
@@ -88,7 +101,6 @@ for ($counter = 1; $counter <= $message_count; $counter++) {
                         $card = pg_fetch_assoc($card_query);
                         $card_id = $card['id'];
                     } else {
-                        $body = imap_fetchbody($connection, $counter, 1);
                         // To email address is for specific card then insert the email as card comment
                         $val_arr = array(
                             $card_id
@@ -103,7 +115,7 @@ for ($counter = 1; $counter <= $message_count; $counter++) {
                             $list_id,
                             $board_id,
                             'add_comment',
-                            decode_qprint($body)
+                            $body
                         );
                         // Insert email content as comment in respective card
                         pg_query_params($db_lnk, 'INSERT INTO activities (created, modified, card_id, user_id, list_id, board_id, type, comment) VALUES (now(), now(), $1, $2, $3, $4, $5, $6)', $val_arr);
@@ -201,10 +213,42 @@ for ($counter = 1; $counter <= $message_count; $counter++) {
 }
 // Closing the imap connection
 imap_expunge($connection);
-function decode_qprint($str)
-{
-    $str = preg_replace("/\=([A-F][A-F0-9])/", "%$1", $str);
-    $str = urldecode($str);
-    $str = utf8_encode($str);
-    return $str;
+function imapBodyDecode($mbox,$mid,$p,$partno) {
+    // $partno = '1', '2', '2.1', '2.1.3', etc for multipart, 0 if simple
+    $message = '';
+    // DECODE DATA
+    $data = ($partno)?
+        imap_fetchbody($mbox,$mid,$partno):  // multipart
+        imap_body($mbox,$mid);  // simple
+    // Any part may be encoded, even plain text messages, so check everything.
+    if ($p->encoding==4) {
+        $data = quoted_printable_decode($data);
+    } elseif ($p->encoding==3) {
+        $data = base64_decode($data);
+    }
+    
+    // TEXT
+    if ($p->type==0 && $data) {
+        // Messages may be split in different parts because of inline attachments,
+        // so append parts together with blank row.
+        if (strtolower($p->subtype)=='plain') {
+            $message .= trim($data) ."\n\n";
+        } else {
+            $message .= $data ."<br><br>";
+        }
+    }
+    // EMBEDDED MESSAGE
+    // Many bounce notifications embed the original message as type 2,
+    // but AOL uses type 1 (multipart), which is not handled here.
+    // There are no PHP functions to parse embedded messages,
+    // so this just appends the raw source to the main message.
+    elseif ($p->type==2 && $data) {
+        $message .= $data."\n\n";
+    }
+    // SUBPART RECURSION
+    if ($p->parts) {
+        foreach ($p->parts as $partno0=>$p2)
+            $message[] = imapBodyDecode($mbox,$mid,$p2,$partno.'.'.($partno0+1));  // 1.2, 1.2.1, etc.
+    }
+    return $message;
 }
