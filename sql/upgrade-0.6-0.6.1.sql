8da67cb79fcd73af866ec3b151c1674876a5ea4c
S.Sivachidambaram <s.sivachidambaram@agriya.in>
2018-01-18 12:48:03 +0530

    Fixes #1495: Gmail Add-on OAuth client created

--- sql/upgrade-0.6-0.6.1.sql
+++ sql/upgrade-0.6-0.6.1.sql
@@ -4,3 +4,5 @@ UPDATE acl_board_links SET url = '/boaards/?/lists/?', method = 'PUT' WHERE slug
 UPDATE acl_board_links SET url = '/boards/?/lists/?/cards/?', method = 'PUT' WHERE slug = 'send_back_to_archived_card';
 UPDATE acl_board_links SET url = '/boards/?/lists/?' WHERE slug = 'send_back_to_archived_list';
 UPDATE users SET is_send_newsletter = 2, timezone = (SELECT value FROM settings WHERE name = 'SITE_TIMEZONE') WHERE id = 1;
+
+INSERT INTO "oauth_clients" ("client_id", "client_secret", "redirect_uri", "grant_types", "scope", "user_id", "client_name", "client_url", "logo_url", "tos_url", "policy_url", "modified", "created") VALUES ('6728003996146168', '1xqu3wl3bhwffs7j9polccgce2', '', 'client_credentials refresh_token authorization_code', NULL, NULL, 'Gmail Add-on', '', NULL, NULL, NULL, '2018-01-18 12:18:03', '2018-01-18 12:18:03');
