771225dc16883f0e9ed2f2e73330887729db5c83
Ahsan <l.marikannan@agriya.in>
2018-03-10 13:20:42 +0530

    Fixes #1640: While change ElasticSearch index, need to delete tmp file

--- server/php/R/r.php
+++ server/php/R/r.php
@@ -4869,6 +4869,14 @@ function r_post($r_resource_cmd, $r_resource_vars, $r_resource_filters, $r_post)
                             break;
                         }
                     }
+                    if (strpos($key, 'r_elasticsearch_index_name') !== false) {
+                        if(trim(strtolower($app['settings'][$key]['value'])) !== trim(strtolower($val))) {
+                            $filename = APP_PATH . '/tmp/cache/r_elasticsearch_last_processed_activity_id.php';
+                            if (file_exists($filename)) {
+                                unlink($filename);
+                            }
+                        }
+                    }
                     $app['settings'][$key]['value'] = $val;
                 }
                 $fh = fopen(APP_PATH . '/client/apps/' . $folder_name . '/app.json', 'w');
