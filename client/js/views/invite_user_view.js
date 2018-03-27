b89fbf813e558ed3b0df5f4973a4de6414822f0d
marikannan l <marikannanl308@gmail.com>
2018-03-27 18:35:55 +0530

    issue #1343: invite user integration going on

---
+++ client/js/views/invite_user_view.js
@@ -0,0 +1,133 @@
+/**
+ * @fileOverview This file has functions related to show all visibility view. This view calling from board add view, board header view and board view.
+ * Available Object:
+ *	App.boards						: this object contain all boards(Based on logged in user)
+ *	this.model						: visibility(String).
+ */
+if (typeof App === 'undefined') {
+    App = {};
+}
+/**
+ * InviteUser View
+ * @class InviteUserView
+ * @constructor
+ * @extends Backbone.View
+ */
+App.InviteUserView = Backbone.View.extend({
+    tagName: 'li',
+    /**
+     * Constructor
+     * initialize default values and actions
+     */
+    initialize: function() {
+        if (!_.isUndefined(this.model) && this.model !== null) {
+            this.model.showImage = this.showImage;
+        }
+        this.render();
+    },
+    template: JST['templates/invite_user'],
+    /**
+     * Events
+     * functions to fire on events (Mouse events, Keyboard Events, Frame/Object Events, Form Events, Drag Events, etc...)
+     */
+    events: {
+        'submit #InviteUserForm': 'inviteNewUser',
+        'blur .js-invite-user-email': 'showInfo',
+        'keydown .js-invite-user-email': 'clearInfo',
+        
+    },
+    /**
+     * render()
+     * populate the html to the dom
+     * @param NULL
+     * @return object
+     *
+     */
+    render: function() {
+        this.$el.html(this.template({
+            board: this.model,
+        }));
+        $('.js-filter-parent').addClass('open');
+        this.showTooltip();
+        return this;
+    },
+    /**
+     * clearInfo()
+     * populate the html to the dom
+     * @param NULL
+     * @return object
+     *
+     */
+    clearInfo: function(e) {
+        var target = $(e.target);
+        $(target).closest('form').find('.js-invite-user-info').html('');
+        $(target).closest('form').find('.js-invite-user-info-block').removeClass('show').addClass('hide');
+    },
+    /**
+     * showInfo()
+     * populate the html to the dom
+     * @param NULL
+     * @return object
+     *
+     */
+    showInfo: function(e) {
+        e.preventDefault();
+        var target = $(e.target);
+        var email = $(e.currentTarget).val();
+        var nameMatch = email.match(/^([^@]*)@/);
+        var name = nameMatch ? nameMatch[1] : null;
+        var nameParts = name.split(".");
+        var fullname = nameParts[0].charAt(0).toUpperCase() + nameParts[0].slice(1);
+        if(nameParts.length > 1) {
+            var fullname = nameParts[0].toUpperCase()+' '+nameParts[1].charAt(0).toUpperCase() + nameParts[1].slice(1);
+        }
+        $(target).closest('form').find('.js-invite-user-fullname').val(fullname);
+        $(target).closest('form').find('.js-invite-user-info').html('');
+        $(target).closest('form').find('.js-invite-user-info').html('<strong>'+email+'</strong>? We don\'t know that person. Add a name and click "Send" and we’ll add a virtual member and send them an invite email. They\'ll automatically receive access to the board once they sign up and confirm their email address.');
+        $(target).closest('form').find('.js-invite-user-info-block').removeClass('hide').addClass('show');
+        
+        return false;
+    },
+    /**
+     * inviteNewUser()
+     * invite new user
+     * @param e
+     * @type Object(DOM event)
+     * @return false
+     *
+     */
+    inviteNewUser: function(e) {
+        if (!$.trim($('#inputUserName').val()).length) {
+            $('.error-msg').remove();
+            $('<div class="error-msg text-primary h6">' + i18next.t('Whitespace is not allowed') + '</div>').insertAfter('#inputUserName');
+        } else {
+            $('.error-msg').remove();
+            e.preventDefault();
+            var target = $(e.target);
+            var data = target.serializeObject();
+            var self = this;
+            var user = new App.User();
+            user.url = api_url + 'users.json';
+            user.save(data, {
+                success: function(model, response) {
+                    if (response.error) {
+                        if (response.error === 1) {
+                            self.flash('danger', i18next.t('Email address already exist. Your registration process is not completed. Please, try again.'));
+                        } else if (response.error === 2) {
+                            self.flash('danger', i18next.t('Username already exists. Your registration process is not completed. Please, try again.'));
+                        }
+                        $('#inputPassword').val('');
+                    } else {
+                        self.flash('success', i18next.t('User added successfully.'));
+                        target[0].reset();
+                        app.navigate('#/users', {
+                            trigger: true,
+                            replace: true
+                        });
+                    }
+                }
+            });
+        }
+        return false;
+    },
+});
