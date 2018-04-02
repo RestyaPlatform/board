2369fe95b06eafe70ca51c59ff77667a30aba1bd
marikannan l <marikannanl308@gmail.com>
2018-04-02 12:50:10 +0530

    Fixes #1668: Single step user add UI for admin

--- server/php/R/r.php
+++ server/php/R/r.php
@@ -1498,7 +1498,7 @@ function r_get($r_resource_cmd, $r_resource_vars, $r_resource_filters)
         }
         break;
 
-        case '/boards/?/lists/?/cards/?/checklists':
+    case '/boards/?/lists/?/cards/?/checklists':
         $sql = 'SELECT row_to_json(d) FROM (SELECT * FROM checklist_add_listing al WHERE board_id = $1) as d ';
         array_push($pg_params, $r_resource_vars['boards']);
         if ($result = pg_query_params($db_lnk, $sql, $pg_params)) {
@@ -2046,9 +2046,7 @@ function r_post($r_resource_cmd, $r_resource_vars, $r_resource_filters, $r_post)
             $table_name = 'users';
             $r_post['password'] = getCryptHash($r_post['password']);
             $r_post['role_id'] = 2; // user
-            $r_post['initials'] = strtoupper(substr($r_post['username'], 0, 1));
             $r_post['ip_id'] = saveIp();
-            $r_post['full_name'] = email2name($r_post['email']);
             $default_email_notification = 0;
             if (DEFAULT_EMAIL_NOTIFICATION === 'Periodically') {
                 $default_email_notification = 1;
