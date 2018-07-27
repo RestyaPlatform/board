18321b0aa3e9d19bdaa01397628bafec45ce09a0
sridhar v <sridharv391@gmail.com>
2018-07-27 13:53:27 +0530

    Fixes #1973 : Changes for adding download all attachments app added

--- client/js/views/card_view.js
+++ client/js/views/card_view.js
@@ -575,7 +575,6 @@ App.CardView = Backbone.View.extend({
      *
      */
     showCardModal: function(e) {
-        e.preventDefault();
         $('ul.dropdown-menu').parent().removeClass('open');
         if (this.model === null || _.isEmpty(this.model)) {
             return false;
