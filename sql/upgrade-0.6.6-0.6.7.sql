878ecac6822237380ccb5fa58e5d13e4394555ab
saravanan_477at17 <s.saravanan@agriya.in>
2018-11-21 13:07:04 +0530

    Fixes #2218: Permission for boards label listing added

--- sql/upgrade-0.6.6-0.6.7.sql
+++ sql/upgrade-0.6.6-0.6.7.sql
@@ -31,3 +31,20 @@ VALUES (now(), now(), (select id from acl_board_links where slug='view_card_isti
 
 INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
 VALUES (now(), now(), (select id from acl_board_links where slug='view_card_isting'), '2');
+
+DELETE FROM "acl_board_links_boards_user_roles" WHERE acl_board_link_id = (select id from acl_board_links where slug='view_board_label_isting');
+
+DELETE FROM "acl_board_links" WHERE slug = 'view_board_label_isting';
+
+SELECT pg_catalog.setval('acl_board_links_seq', (SELECT MAX(id) FROM acl_board_links), true);
+
+INSERT INTO "acl_board_links" ("created", "modified", "name", "url", "method", "slug", "group_id", "is_hide")
+VALUES (now(), now(), 'Boards labels listing', '/boards/?/labels', 'GET', 'view_board_label_isting', '4', '0');
+
+SELECT pg_catalog.setval('acl_board_links_boards_user_roles_seq', (SELECT MAX(id) FROM acl_board_links_boards_user_roles), true);
+
+INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
+VALUES (now(), now(), (select id from acl_board_links where slug='view_board_label_isting'), '1');
+
+INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
+VALUES (now(), now(), (select id from acl_board_links where slug='view_board_label_isting'), '2');
