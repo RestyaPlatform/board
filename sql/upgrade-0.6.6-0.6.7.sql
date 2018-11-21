6dad490ff21b7e3c25740b8f91b2bd8764121ba6
saravanan_477at17 <s.saravanan@agriya.in>
2018-11-21 12:57:24 +0530

    Fixes #2218:  Added the permission to get boards lists and boards card for the normal users in the board

---
+++ sql/upgrade-0.6.6-0.6.7.sql
@@ -0,0 +1,33 @@
+DELETE FROM "acl_board_links_boards_user_roles" WHERE acl_board_link_id = (select id from acl_board_links where slug='get_board_lists');
+
+DELETE FROM "acl_board_links" WHERE slug = 'get_board_lists';
+
+SELECT pg_catalog.setval('acl_board_links_seq', (SELECT MAX(id) FROM acl_board_links), true);
+
+INSERT INTO "acl_board_links" ("created", "modified", "name", "url", "method", "slug", "group_id", "is_hide")
+VALUES (now(), now(), 'Get Board Lists', '/boards/?/lists', 'GET', 'get_board_lists', '3', '0');
+
+SELECT pg_catalog.setval('acl_board_links_boards_user_roles_seq', (SELECT MAX(id) FROM acl_board_links_boards_user_roles), true);
+
+INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
+VALUES (now(), now(), (select id from acl_board_links where slug='get_board_lists'), '1');
+
+INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
+VALUES (now(), now(), (select id from acl_board_links where slug='get_board_lists'), '2');
+
+DELETE FROM "acl_board_links_boards_user_roles" WHERE acl_board_link_id = (select id from acl_board_links where slug='view_card_isting');
+
+DELETE FROM "acl_board_links" WHERE slug = 'view_card_isting';
+
+SELECT pg_catalog.setval('acl_board_links_seq', (SELECT MAX(id) FROM acl_board_links), true);
+
+INSERT INTO "acl_board_links" ("created", "modified", "name", "url", "method", "slug", "group_id", "is_hide")
+VALUES (now(), now(), 'Get Board Lists', '/boards/?/lists/?/cards/?', 'GET', 'view_card_isting', '4', '0');
+
+SELECT pg_catalog.setval('acl_board_links_boards_user_roles_seq', (SELECT MAX(id) FROM acl_board_links_boards_user_roles), true);
+
+INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
+VALUES (now(), now(), (select id from acl_board_links where slug='view_card_isting'), '1');
+
+INSERT INTO "acl_board_links_boards_user_roles" ("created", "modified", "acl_board_link_id", "board_user_role_id")
+VALUES (now(), now(), (select id from acl_board_links where slug='view_card_isting'), '2');
