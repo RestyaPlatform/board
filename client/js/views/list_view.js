368428c9a63dadd54b98806580110bdd6624fddd
saravanan_477at17 <s.saravanan@agriya.in>
2019-03-08 11:16:16 +0530

    Fixes #2026:  Card count updated double in work progress limit issue fixed

--- client/js/views/list_view.js
+++ client/js/views/list_view.js
@@ -1614,6 +1614,8 @@ App.ListView = Backbone.View.extend({
                         global_uuid[data.uuid] = options.temp_id;
                         card.set('id', data.uuid);
                     }
+                    var cards_count = isNaN(self.model.attributes.card_count) ? 0 : self.model.attributes.card_count;
+                    self.model.set('card_count', parseInt(cards_count) + 1);
                     if (parseInt(self.model.attributes.card_count) === 1) {
                         // Removing the &nbsp; in the card listing after adding card
                         $('#js-card-listing-' + self.model.id).html(function(i, h) {
