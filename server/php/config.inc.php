19da49482ea319987fa6322d6f497b2023c9cf56
G Balraj <g.balraj@agriya.in>
2019-03-08 10:25:05 +0530

    Changes #plateformsh:  Plateform.sh changes are added

--- server/php/config.inc.php
+++ server/php/config.inc.php
@@ -26,10 +26,10 @@ if (!defined('JSON_UNESCAPED_UNICODE')) {
 }
 define('APP_PATH', dirname(dirname(dirname(__FILE__))));
 define('DS', DIRECTORY_SEPARATOR);
-define('MEDIA_PATH', APP_PATH . DS . 'media');
-define('TMP_PATH', APP_PATH . DS . 'tmp');
-define('CACHE_PATH', APP_PATH . DS . 'tmp' . DS . 'cache');
-define('IMG_PATH', APP_PATH . DS . 'client' . DS . 'img');
+define('MEDIA_PATH', APP_PATH . DS .'media');
+define('TMP_PATH', APP_PATH . DS .'tmp');
+define('CACHE_PATH', APP_PATH . DS .'tmp'. DS .'cache');
+define('IMG_PATH', APP_PATH . DS .'client'. DS .'img');
 define('PLUGIN_PATH', APP_PATH . DS . 'server' . DS . 'php' . DS . 'plugins');
 define('SITE_URL_FOR_SHELL', CACHE_PATH . DS . 'site_url_for_shell.php');
 define('CLIENT_INFORMATION', CACHE_PATH . DS . 'client_information.php');
@@ -41,11 +41,26 @@ if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
     define('OAUTH_CLIENTID', '7742632501382313');
     define('OAUTH_CLIENT_SECRET', '4g7C4l1Y2b0S6a7L8c1E7B3K0e');
 }
-define('R_DB_HOST', 'localhost');
-define('R_DB_USER', 'restya');
-define('R_DB_PASSWORD', 'hjVl2!rGd');
-define('R_DB_NAME', 'restyaboard');
-define('R_DB_PORT', 5432);
+if (getenv('PLATFORM_RELATIONSHIPS')) {
+    $relationships = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')), true);
+    if (!empty($relationships['database'])) {
+       foreach ($relationships['database'] as $endpoint) {
+             define('R_DB_HOST', $endpoint['host']);
+             define('R_DB_USER', $endpoint['username']);
+             define('R_DB_PASSWORD', $endpoint['password']);
+             define('R_DB_NAME', $endpoint['path']);
+             define('R_DB_PORT', $endpoint['port']);
+          break;
+       }
+    }
+ } else {
+    define('R_DB_HOST', 'localhost');
+    define('R_DB_USER', 'restya');
+    define('R_DB_PASSWORD', 'hjVl2!rGd');
+    define('R_DB_NAME', 'restyaboard');
+    define('R_DB_PORT', 5432);
+ }
+
 define('SECURITYSALT', 'e9a556134534545ab47c6c81c14f06c0b8sdfsdf');
 define('SITE_LICENSE_KEY', 'REPLACE YOUR LICENCE HERE');
 if (!defined('STDIN') && !file_exists(SITE_URL_FOR_SHELL) && !empty($_server_domain_url)) {
---
+++ server/php/install.php
@@ -0,0 +1,22 @@
+<?php
+exec('mkdir -p /tmp/pgpass');
+exec('chmod -R go-rwx /tmp/pgpass');
+$relationships = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')), true);
+if (!empty($relationships['database'])) {
+    foreach ($relationships['database'] as $endpoint) {
+          define('R_DB_HOST', $endpoint['hostname']);
+          define('R_DB_USER', $endpoint['username']);
+          define('R_DB_PASSWORD', $endpoint['password']);
+          define('R_DB_NAME', $endpoint['path']);
+          define('R_DB_PORT', $endpoint['port']);
+       break;
+    }
+ }
+echo exec(R_DB_HOST.':'.R_DB_PORT.':'.R_DB_NAME.':'.R_DB_USER.':'.R_DB_PASSWORD.' > /tmp/pgpass/.pgpass');
+exec('chmod go-rwx /tmp/pgpass/.pgpass');
+exec('export PGPASSFILE=/tmp/pgpass/.pgpass');
+exec('psql --port='.R_DB_PORT.' --dbname='.R_DB_NAME.' --username='.R_DB_USER.' --no-password --file=../sql/restyaboard_with_empty_data.sql');
+exec('rm -rf /tmp/pgpass');
+exec('touch tmp/cache/.sql.installed');
+
+
