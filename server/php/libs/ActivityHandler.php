c20889bbc8cff59e2fc8c632d539a8692dbf56af
saravanan_477at17 <s.saravanan@agriya.in>
2019-03-13 13:13:37 +0530

    Changes #2485: Estimated time and Spen time not updated in modal & list for other user browsers issue fixed

--- server/php/libs/ActivityHandler.php
+++ server/php/libs/ActivityHandler.php
@@ -82,15 +82,15 @@ class ActivityHandler
             );
             $obj['card'] = executeQuery('SELECT * FROM cards_listing WHERE id = $1', $obj_val_arr);
             $obj['card']['card_attachments'] = executeQuery('SELECT * FROM card_attachments WHERE card_id = $1', $obj_val_arr);
-            if (is_plugin_enabled('r_custom_fields')){
+            if (is_plugin_enabled('r_custom_fields')) {
                 $obj['custom_fields'] = array();
-                    $conditions = array(
-                        $obj['card']['board_id']
-                    );
-                    $custom_fields = pg_query_params($db_lnk, 'SELECT * FROM custom_fields_listing WHERE board_id IS NULL or board_id = $1 ORDER BY position ASC', $conditions);
-                    while ($custom_field = pg_fetch_assoc($custom_fields)) {
-                        $obj['custom_fields'][] = $custom_field;
-                    }
+                $conditions = array(
+                    $obj['card']['board_id']
+                );
+                $custom_fields = pg_query_params($db_lnk, 'SELECT * FROM custom_fields_listing WHERE board_id IS NULL or board_id = $1 ORDER BY position ASC', $conditions);
+                while ($custom_field = pg_fetch_assoc($custom_fields)) {
+                    $obj['custom_fields'][] = $custom_field;
+                }
             }
         } else if ($obj_type === 'add_card_checklist') {
             $obj_val_arr = array(
