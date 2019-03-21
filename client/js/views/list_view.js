c3bfeb3d83719caafa56553f9baa67d8627a9e5c
saravanan_477at17 <s.saravanan@agriya.in>
2019-03-21 20:15:41 +0530

    Changes #2485: While adding the card in the one browser and changing the view from the list view to grid view in other browser, the card is not added issue fixed

--- client/js/views/list_view.js
+++ client/js/views/list_view.js
@@ -1213,11 +1213,11 @@ App.ListView = Backbone.View.extend({
                     if ($('#js-card-' + e.attributes.id).length === 1) {
                         $('#js-card-' + e.attributes.id).replaceWith(view.render().el);
                     } else {
-                        filtered_cards = self.model.collection.board.cards.where({
+                        filtered_cards = self.model.board.cards.where({
                             is_archived: 0,
                             list_id: parseInt(self.model.id)
                         });
-                        if (filtered_cards.length === 1 || self.model.collection.board.cards.length === 0) {
+                        if (filtered_cards.length === 1 || self.model.board.cards.length === 0) {
                             $('#js-card-listing-' + e.attributes.list_id).append(view.render().el);
                         } else {
                             if (sort_by !== null && sort_by !== null) {
