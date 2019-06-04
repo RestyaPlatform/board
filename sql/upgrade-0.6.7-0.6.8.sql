83db9b1d6f6fc99be35cb4992c8648aafadca32f
saravanan_477at17 <s.saravanan@agriya.in>
2019-06-04 15:05:56 +0530

    Fixes #2406: SQL filename changes added

--- sql/upgrade-0.6.7-0.6.8.sql
+++ sql/upgrade-0.6.7-0.6.8.sql
@@ -1,2 +1,7 @@
+ALTER TABLE "users"
+ALTER "is_productivity_beats" TYPE boolean,
+ALTER "is_productivity_beats" SET DEFAULT 'true',
+ALTER "is_productivity_beats" SET NOT NULL;
+UPDATE "users" SET "is_productivity_beats" = '1';
 INSERT INTO "settings" ("setting_category_id", "setting_category_parent_id", "name", "value", "description", "type", "options", "label", "order")
 VALUES ('17', '0', 'CALENDAR_VIEW_CARD_COLOR', 'Default Color', NULL, 'select', 'Card Color,Label Color,Default Color', 'Calendar View Card Color ', '4')
