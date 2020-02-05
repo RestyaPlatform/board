bbaeca76ece96437b2232b920a9723d5d76370eb
S SARAVANAN <s.saravanan@agriya.in>
2020-02-05 10:32:12 +0530

    Fixes #3407: MyRestyaboard iOS app - boards not loaded even after connecting instance issue going on

--- client/js/views/application_view.js
+++ client/js/views/application_view.js
@@ -56,7 +56,10 @@ App.ApplicationView = Backbone.View.extend({
                 $.cookie('music_play', "1");
             }
         }
-        localforage.config({ driver: [localforage.WEBSQL, localforage.INDEXEDDB, localforage.LOCALSTORAGE], name: 'WebSQL-Rox' });
+        localforage.config({
+            driver: [localforage.WEBSQL, localforage.INDEXEDDB, localforage.LOCALSTORAGE],
+            name: 'WebSQL-Rox'
+        });
         localforage.getItem('links', function(err, value) {
             if (value) {
                 if (role_links.length === 0 && value !== undefined && value !== null) {
